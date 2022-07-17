c4202994766946653a8feef4985bab7b3c736c09
ehteht487 <92257659+ehteht487@users.noreply.github.com>
2022-07-17 23:28:18 +0200

    Added javascript

--- js/recipes.js
+++ js/recipes.js
@@ -1,58 +1,46 @@
 const postContainer = document.querySelector(".post-container");
+const searchButton = document.querySelector(".search-button");
+
+
 
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'c7fa2aa016msh1cd3a9b66b694a3p11980ejsnf2e4c69523b3',
-		'X-RapidAPI-Host': 'edamam-recipe-search.p.rapidapi.com'
+		'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
 	}
-}
+};
 
-const baseUrl = "https://edamam-recipe-search.p.rapidapi.com/search?q=pasta";
-
-async function fetchPosts() {
-    const response = await fetch(baseUrl, options);
-    const results = await response.json();
-
-    
-
-    const posts = results.hits
-     console.log(posts)
-     
-     posts.forEach(function(post) {
-        postContainer.innerHTML += `<div>
-                                    <a href="recipespesific.html?id=${post.recipe.ingredients}" class="product">
-                                    <h4>${post.recipe.label}</h4>
-                                    <img src="${post.recipe.image}">
-                                    </a>
-
-                                    </div>`
-
-     })
-}
-
-fetchPosts();
-
-const searchButton = document.querySelector(".search-button");
 
 searchButton.onclick = function() {
-    const searchInput = document.querySelector("#search").value;
-    const newUrl = url + `?q=${searchInput}`;
-    postContainer.innerHTML = "";
-    fetchPosts(newUrl);    
+    const searchInput = document.querySelector("#search-input").value
+    const allergens = document.querySelector("#allergens").value
+    const extraIngredients = document.querySelector("#extra-ingredients").value
+
+    const api = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=${searchInput}&intolerances=${allergens}&includeIngredients=${extraIngredients}&instructionsRequired=true&fillIngredients=false&addRecipeInformation=true&ignorePantry=true&sort=healthiness&sortDirection=asc&number=50`
+
+
+    async function fetchPosts() {
+        
+        const response = await fetch(api, options);
+        const result = await response.json(); 
+        
+        console.log(result)
+
+        postContainer.innerHTML += ``
+
+        result.results.forEach(function(recipe){
+        postContainer.innerHTML += `<div class="card"> 
+                                        <a href="recipespesific.html?id=${recipe.id}" class="nav-other">${recipe.id}</a>
+
+                                        <h3>${recipe.title}</h3>
+                                        <img src="${recipe.image}">                                      
+                                    </div>
+                                    `
+
+    })
+    }
+
+    fetchPosts(api);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
--- js/recipespesific.js
+++ js/recipespesific.js
@@ -1,42 +1,41 @@
+const recipeContainer = document.querySelector(".recipe-container");
+
 const queryString = document.location.search;
 const params = new URLSearchParams(queryString);
-const id = params.get("id")
+const id = params.get("id");
 
 console.log(id)
-
-
-
-const postContainer = document.querySelector(".post-container");
 
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'c7fa2aa016msh1cd3a9b66b694a3p11980ejsnf2e4c69523b3',
-		'X-RapidAPI-Host': 'edamam-recipe-search.p.rapidapi.com'
+		'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
 	}
-}
+};
 
-const baseUrl = "https://edamam-recipe-search.p.rapidapi.com/search?q=pasta" + id;
+
+const api = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/"+id+"/information/"
+
 
 async function fetchPosts() {
-    const response = await fetch(baseUrl, options);
-    const results = await response.json();
+    const response = await fetch(api, options);
+    const result = await response.json(); 
+        
+    console.log(result)
+    createInfo(result)
 
-    
 
-    const posts = results.hits
-     console.log(posts)
+};
 
-     postContainer.innerHTML = `<div>
-                                    
-                                    <h4>${results.recipe.label}</h4>
-                                    <img src="${results.recipe.image}">
-                                     </a>
 
-                                     </div>`
-                                
-     
-     
+fetchPosts(api);
+
+function createInfo(result) {
+    recipeContainer.innerHTML = `<div class="recipes">
+                                <h3>${result.title}</h3>
+                                <img src="${result.image}">
+                                <p class="recipes-content">${result.instructions}</p>
+                                </div>`
 }
 
-fetchPosts();
